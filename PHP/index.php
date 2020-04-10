cc212c02fa2181f5613044ffb475d14987458b91
roubilibo <44028475+roubilibo@users.noreply.github.com>
2020-04-10 15:06:33 +0700

    tugas web
    menyelesaikan tugas web

---
+++ PHP/index.php
@@ -0,0 +1,15 @@
+<?php
+$thn_lahir = 2002;
+$thn_skrg = 2019;
+$usia = $thn_skrg - $thn_lahir;
+?>
+<!DOCTYPE html>
+<html>
+
+<body>
+    <p>Berapakah usiamu?
+        Usiaku <?php echo $usia; ?> tahun
+    </p>
+</body>
+
+</html>
